40eef02e7ae4708f137f8257b2aa7e0a5f81d0c7
beshug7 <kai123fall@gmail.com>
2025-04-02 19:19:23 +0530

    Fix MongoDB URI variable inconsistency

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || process.env.MONGODB_URI;
 
 if (!MONGO_URI) {
   throw new Error(
-    'Please define the MONGO_URI environment variable inside .env.local'
+    'Please define the MONGO_URI or MONGODB_URI environment variable inside .env.local'
   );
 }
 
